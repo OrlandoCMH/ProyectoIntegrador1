949214ea0f0415608568e84adb90f329252abf02
OrlandoCM <germanjvi34@gmail.com>
2024-10-17 08:55:31 -0600

    Se agrego un h1

--- index.php
+++ index.php
@@ -71,6 +71,7 @@ if (!empty($_SESSION['activa'])) {
       <div class="col" style="background-color: black; padding: 25px;"> <!-- Columna2 -->
         <div class="row">
           <h1 style="color: grey;  text-align: center; color:white;">AUTENTIFICACIÓN</h1>
+          <h1>HOLA MUNDO</h1>
         </div>
 
         <form style="padding: 25px;" method="POST">
